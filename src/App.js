c1ee2bee94f3227f552de9b2e7e42d99a3f0696f
Joseph C <jciv.coding@gmail.com>
2018-10-02 11:25:06 -0400

    CSS Grid & React Changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// import TreeExample from "./menu/mainmenu.js";
+import Tree from './filetree/components/Tree';
 
 class App extends Component {
   render() {
@@ -12,9 +12,14 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
 
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='tree'>
+          <Tree />
+        </div>
+
+        <div className='footer'>
+          <p> Footer. </p>
+        </div>
+
       </div>
     );
   }
---
+++ src/filetree/components/Tree.js
@@ -0,0 +1,95 @@
+// "use strict";
+
+import React from 'react';
+import Folder from './Folder';
+import File from './File';
+
+
+class Tree extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    };
+  }
+
+  // componentDidMount() {
+  //   // This is where we would change the state of what's been rendered to the DOM below
+  // }
+
+  render() {
+    return (
+      <React.Fragment>
+        This is the tree component.
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+        <button> Folder/File </button><br/>
+      </React.Fragment>
+    )
+  }
+}
+
+
+export default Tree;
